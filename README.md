399a30be44358cf08046ab249baeaeacebc2b55b
dustin.kelly <dustin.kelly@2checkout.com>
2020-03-17 09:31:43 -0400

    Initial script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# coronavirus-heatmap
+# Coronavirus Confirmed Cases by US State Heatmap
+
+Clone this repository and run coronavirus_heatmap.py to scrape case data from wikipedia and save an interactive .html heatmap in the same directory as the script file.
+
+Requires plotly, pandas, and requests.
---
+++ coronavirus_heatmap.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Import modules
+import pandas as pd
+import plotly.express as px
+import plotly
+import requests
+
+# Fetch case data from wikipedia
+url = 'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_the_United_States'
+
+data = pd.read_html(requests.get(url).content)[4]
+data.columns = ['Drop','State','Cases','Recoveries','Deaths','Source']
+
+# Fetch state abbreviations from wikipedia
+url2 = 'https://en.wikipedia.org/wiki/List_of_U.S._state_abbreviations'
+states = pd.read_html(requests.get(url2).content)[0]
+states.columns = ['Region','Status','ISO','ANSI','ANSI2','USPS','USCG','GPO','AP','Other']
+states = states.query('Status == "State"').reset_index(drop = True).filter(items = ['Region','USPS'])
+
+# Filter and clean data
+state_names = states['Region']
+
+p_data = (data.query('State in @state_names')
+            .reset_index(drop=True)
+            .filter(items=['State','Cases'])
+            .merge(states, how = 'left', left_on = ['State'], right_on = ['Region'])
+            .filter(items = ['USPS','Cases']))
+
+p_data['Cases'] = pd.to_numeric(p_data['Cases'])
+p_data.columns = ['State','Cases']
+
+# Plot and save choropleth map
+df = p_data
+
+fig = px.choropleth(df, locations= "State",
+                    locationmode="USA-states",
+                    color= "Cases",
+                    scope="usa",
+                    range_color=[p_data['Cases'].min(),p_data['Cases'].max()],
+                    color_continuous_scale=px.colors.sequential.thermal)
+
+fig.update_layout(title = 'Coronavirus Confirmed Cases in the US by State')
+
+plotly.offline.plot(fig, filename = 'Coronavirus_Map.html', auto_open=False)
